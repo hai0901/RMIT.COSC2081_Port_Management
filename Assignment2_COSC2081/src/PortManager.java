c2c9baa018ca3ade212c39576c691d3544295ff0
s3979628 <119097888+s3979628@users.noreply.github.com>
2023-09-09 14:20:36 +0700

    create User class class
    create user class with port manager and sysem admin, but did not add any function

---
+++ Assignment2_COSC2081/src/PortManager.java
@@ -0,0 +1,5 @@
+public class PortManager extends User{
+    public PortManager(String username, String password) {
+        super(username, password);
+    }
+}
---
+++ Assignment2_COSC2081/src/SystemAdmin.java
@@ -0,0 +1,7 @@
+public class SystemAdmin extends User {
+
+    public SystemAdmin(String username, String password) {
+        super(username, password);
+    }
+
+}
