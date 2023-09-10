a1d5a824fa77d98d640ab972c6dad4744f4abb44
s3979628 <119097888+s3979628@users.noreply.github.com>
2023-09-10 16:12:37 +0700

    Add method Calculate Fuel

--- Assignment2_COSC2081/src/Vehicle.java
+++ Assignment2_COSC2081/src/Vehicle.java
@@ -13,6 +13,8 @@ public class Vehicle {
     private int openSideContainers;
     private int refrigeratedContainers;
     private int liquidContainers;
+    private Location location;
+
 
 
     public Vehicle(String name, double currentFuel, double capacityFuel, Port currentPort, int container, String vehID, int carryingCapacity) {
@@ -105,6 +107,54 @@ public class Vehicle {
         }
         if (container.getConType() == ContainerType.LIQUID){
             liquidContainers--;
+        }
+    }
+    public double calculateFuelConsumption(Port destinationPort,ContainerType containerType){
+        double fuelConsumptionRate;
+        if (this instanceof Ship) {
+            fuelConsumptionRate = getFuelConsumptionRateShip(containerType);
+        } else if (this instanceof Truck) {
+
+            fuelConsumptionRate = getFuelConsumptionRateTruck(containerType);
+        } else {
+            fuelConsumptionRate = 1; //The default value for other vehicles if needed
+        }
+        double fuelConsumption = fuelConsumptionRate * location.calculateDistance(destinationPort.getLocation());
+
+        return fuelConsumption;
+
+    }
+    private double getFuelConsumptionRateShip(ContainerType containerType) {
+        switch (containerType) {
+            case DRY_STORAGE:
+                return 3.5;
+            case OPEN_TOP:
+                return 2.8;
+            case OPEN_SIDE:
+                return 2.7;
+            case REFRIGERATED:
+                return 4.5;
+            case LIQUID:
+                return 4.8;
+            default:
+                return 0.0; // Default rate for unknown type
+        }
+    }
+
+    private double getFuelConsumptionRateTruck(ContainerType containerType) {
+        switch (containerType) {
+            case DRY_STORAGE:
+                return 4.6;
+            case OPEN_TOP:
+                return 3.2;
+            case OPEN_SIDE:
+                return 3.2;
+            case REFRIGERATED:
+                return 5.4;
+            case LIQUID:
+                return 5.3;
+            default:
+                return 0.0; // Default rate for unknown type
         }
     }
 
