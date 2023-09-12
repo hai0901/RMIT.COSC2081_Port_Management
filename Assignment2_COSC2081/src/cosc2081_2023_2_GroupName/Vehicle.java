91b669561b886dfd0b8352a277f23bece4a49d0c
QuagKai <119025914+QuagKai@users.noreply.github.com>
2023-09-12 23:31:13 +0700

    - Fix correct variables'name

--- Assignment2_COSC2081/src/cosc2081_2023_2_GroupName/Vehicle.java
+++ Assignment2_COSC2081/src/cosc2081_2023_2_GroupName/Vehicle.java
@@ -1,65 +1,84 @@
 package cosc2081_2023_2_GroupName;
 
 
-public class Vehicle {
+public abstract class Vehicle {
     private String name;
-    private double curFuel;
-    private double capFuel;
-    private Port curPort;
-    private Integer numContainer;
+    private double currentFuel;
+    private double capacityFuel;
+    private Port currentPort;
+    private int container;
 
     Vehicle(){
         this.name = "none";
-        this.curFuel = 0;
-        this.capFuel = 0;
-        this.curPort = null;
-        this.numContainer = 0;
+        this.currentFuel = 0;
+        this.capacityFuel = 0;
+        this.currentPort = null;
+        this.container = 0;
     }
 
-    Vehicle(String name, double curFuel, double capFuel, Port port, Integer numContainer) {
+    Vehicle(String name, double currentFuel, double capacityFuel, Port port, int container) {
         this.name = name;
-        this.curFuel = curFuel;
-        this.capFuel = capFuel;
-        this.curPort = port;
-        this.numContainer = numContainer;
+        this.currentFuel = currentFuel;
+        this.capacityFuel = capacityFuel;
+        this.currentPort = port;
+        this.container = container;
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
 
-    public double getCurFuel() {
-        return this.curFuel;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public double getCapFuel() {
-        return this.capFuel;
+    public double getCurrentFuel() {
+        return currentFuel;
     }
 
-    public Port getCurPort() {
-        return this.curPort;
+    public void setCurrentFuel(double currentFuel) {
+        this.currentFuel = currentFuel;
     }
 
-    public Integer getNumContainer() {
-        return this.numContainer;
+    public double getCapacityFuel() {
+        return capacityFuel;
+    }
+
+    public void setCapacityFuel(double capacityFuel) {
+        this.capacityFuel = capacityFuel;
+    }
+
+    public Port getCurrentPort() {
+        return currentPort;
+    }
+
+    public void setCurrentPort(Port currentPort) {
+        this.currentPort = currentPort;
+    }
+
+    public int getContainer() {
+        return container;
+    }
+
+    public void setContainer(int container) {
+        this.container = container;
     }
 
     @Override
     public String toString() {
         return "Vehicle{" +
                 "name='" + name + '\'' +
-                ", curFuel=" + curFuel +
-                ", capFuel=" + capFuel +
-                ", curPort=" + curPort +
-                ", numContainer=" + numContainer +
-                '}';
+                ", currentFuel=" + currentFuel +
+                ", capacityFuel=" + capacityFuel +
+                ", currentPort=" + currentPort +
+                ", container=" + container;
     }
 }
 
 class Truck extends Vehicle {
-   private Integer trNumber;
+   private String trNumber;
    private String type;
-   Truck(String name, double curFuel, double capFuel, Port port, Integer numContainer,Integer trNumber, String type) {
+   Truck(String name, double curFuel, double capFuel, Port port, Integer numContainer,String trNumber, String type) {
        super(name, curFuel, capFuel, port, numContainer);
        this.trNumber = trNumber;
        boolean inserted = false;
@@ -75,36 +94,25 @@ class Truck extends Vehicle {
        else System.out.println("This truck type is not accepted");
    }
 
-    @Override
-    public String toString() {
-        return "Truck{" +
-                "name= '" + super.getName() + '\'' +
-                ", curFuel= " + super.getCurFuel() +
-                ", capFuel= " + super.getCapFuel() +
-                ", curPort= " + super.getCurPort() +
-                ", numContainer= " + super.getNumContainer() +
-                ", trNumber= " + trNumber +
-                ", type= '" + type + '\'' +
-                '}';
-    }
+   @Override
+   public String toString() {
+       return super.toString() +
+               ", trNumber: " + trNumber +
+               ", type: " + type + "}";
+   }
 }
 class Ship extends Vehicle {
-    private Integer shNumber;
+    private String shNumber;
 
-    Ship(String name, double curFuel, double capFuel, Port port, Integer numContainer,Integer shNumber) {
-        super(name, curFuel, capFuel, port, numContainer);
+    Ship(String name, double curFuel, double capFuel, Port port, int container,String shNumber) {
+        super(name, curFuel, capFuel, port, container);
         this.shNumber = shNumber;
     }
 
+    @Override
     public String toString() {
-        return "Truck{" +
-                "name= '" + super.getName() + '\'' +
-                ", curFuel= " + super.getCurFuel() +
-                ", capFuel= " + super.getCapFuel() +
-                ", curPort= " + super.getCurPort() +
-                ", numContainer= " + super.getNumContainer() +
-                ", trNumber= " + shNumber + '\''+
-                '}';
+        return super.toString() +
+                ", trNumber: " + shNumber + "}";
     }
 }
 
@@ -112,8 +120,8 @@ class TestVehicle {
     public static void main(String[] args) {
         Port A = new Port("A");
         Port B = new Port("B");
-        Vehicle basicTruck = new Truck("Truck1", 100.0, 200.0, A ,3, 1, "basic");
-        Vehicle basicShip = new Ship("Ship1", 1000.0, 2000.0, B ,5, 1);
+        Vehicle basicTruck = new Truck("Truck1", 100.0, 200.0, A ,3, "1", "basic");
+        Vehicle basicShip = new Ship("Ship1", 1000.0, 2000.0, B ,5, "2");
         System.out.println(basicTruck);
         System.out.println(basicShip);
     }
