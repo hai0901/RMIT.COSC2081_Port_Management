140a8e22d773458c3028bb0f6b3d13a9d9eede77
hai0901 <s3979239@rmit.edu.vn>
2023-09-23 17:45:26 +0700

    making autodeleteFile

--- Assignment2_COSC2081/src/FileHandling/File.java
+++ Assignment2_COSC2081/src/FileHandling/File.java
@@ -5,7 +5,15 @@ import User.PortManager;
 import java.io.*;
 import java.io.PrintWriter;
 import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+
+
 
 public class File {
 
@@ -18,6 +26,54 @@ public class File {
         writer.newLine();
         writer.close();
     }
+
+    public static void writeToFileAutoDelete(String fileName, String text, boolean append) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, append));
+        writer.write(text);
+        writer.newLine();
+        writer.close();
+
+        // Schedule line deletion after 1 minute
+        scheduleLineDeletion(fileName, text, 1);
+    }
+
+    private static void scheduleLineDeletion(String fileName, String text, int minutesToKeep) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    removeLineFromFile(fileName, text);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, minutesToKeep * 60 * 1000); // set timmer : 1 minute
+    }
+
+    private static void removeLineFromFile(String fileName, String lineToRemove) throws IOException {
+        Path path = Paths.get(fileName);
+        List<String> lines = Files.readAllLines(path);
+
+        List<String> updatedLines = new ArrayList<>();
+        boolean lineFound = false;
+
+        for (String line : lines) {
+            if (!line.equals(lineToRemove)) {
+                updatedLines.add(line);
+            } else {
+                lineFound = true;
+            }
+        }
+
+        if (lineFound) {
+            // Write the updated lines back to the file
+            Files.write(path, updatedLines, StandardOpenOption.TRUNCATE_EXISTING);
+        }
+    }
+
+
+
 
 
     // Read from file
