51dab53295a780d4c5bed9805208cd202d40bd92
Bummesis <116721067+Bummesis@users.noreply.github.com>
2023-09-23 16:54:49 +0700

    Completing Interface

--- Assignment2_COSC2081/src/Main.java
+++ Assignment2_COSC2081/src/Main.java
@@ -15,6 +15,14 @@ public class Main {
 
         // Display welcome screen
         LoadDataBase.createAll();
+        System.out.println("COSC2081 GROUP ASSIGNMENT");
+        System.out.println("PORT MANAGEMENT MANAGEMENT SYSTEM");
+        System.out.println("Instructor: Mr. Minh Vu & Dr. Phong Ngo");
+        System.out.println("Group: KMPH");
+        System.out.println("s3975831, Ngo Quang Khai");
+        System.out.println("s3979628, Cao Le Hoang Minh");
+        System.out.println("s3963893, Dang Truong Phat");
+        System.out.println("s3979239, Nguyen Pham Tien Hai");
         
         System.out.println("-------------------MAIN  MENU---------------------");
         MainMenu menu = new MainMenu(true);
